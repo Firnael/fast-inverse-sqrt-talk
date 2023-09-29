ad55b7a4b32f9e2ab44d8a2fa05d03cab2b36efe
Audren <audren.burlot@gmail.com>
2023-09-29 17:57:07 +0200

    gros charbon

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-import 'reveal.js/dist/reveal.css';
-// https://revealjs.com/themes/
-import 'reveal.js/dist/theme/blood.css';
-import './styles/hljs/atom-one-dark.min.css';
-import './styles/style.css';
-
 import Reveal from 'reveal.js';
 import RevealNotes from 'reveal.js/plugin/notes/notes';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
 import RevealMath from 'reveal.js/plugin/math/math';
 
-const SLIDE_INDEX_WITH_GAME = '1-0';
+import 'reveal.js/dist/reveal.css';
+// https://revealjs.com/themes/
+import 'reveal.js/dist/theme/blood.css';
+import './styles/hljs/atom-one-dark.min.css';
+import './styles/style.css';
 
 const deck = new Reveal();
 
@@ -26,13 +24,15 @@ deck.initialize({
 //     }
 // });
 
+const SLIDE_INDEX_WITH_GAME = '7-13';
+
 /**
  * Listen for the 'slideChanged' Reveal event, and dispatch events for specific slides
  * - slideChanged is fired when the slide is '1-1' (car mini-game slide)
  * - ...
  */
 const slideChangedEvent = new Event('slideChanged');
-deck.addEventListener('slidechanged', event => {
+deck.addEventListener('slidechanged', (event) => {
     const slideIndex = `${event.indexh}-${event.indexv}`;
     console.log('Slide Index: ', slideIndex);
 
